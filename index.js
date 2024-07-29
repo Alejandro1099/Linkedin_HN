fa45840de800ff89fc64865df5c06e95e9bf3a69
Alejandro <arizaguirre19@gmail.com>
2024-07-29 10:18:06 -0600

    Prueba

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from  'express';
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("servidor de Linkedin HN escuchando");
+    res.send("servidor de Linkedin HN escuchando Buen dia");
 })
 
 app.listen(3000, () => {
