bc74481194cb6952303ef18393a918ba8a8b258f
Alejandro <arizaguirre19@gmail.com>
2024-08-16 11:36:14 -0600

    Prueba con el index.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-    res.end('Funciona con el modulo http')
-});
-
-server.listen(3000);
-console.log('Server on port 3000');
